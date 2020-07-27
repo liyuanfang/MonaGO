693270b992b4433e4ebd2f48196cb580e5b3d482
Hannah Burke <hmbur5@student.monash.edu>
2020-07-27 14:59:54 +1000

    changing port number

--- server/server.py
+++ server/server.py
@@ -390,4 +390,4 @@ def loadConfig():
 if __name__ == '__main__':
     loadConfig()
     loadGOHier()
-    app.run(debug= (config["debug"]=="true"), host="0.0.0.0", port = 80, threaded = True)
+    app.run(debug= (config["debug"]=="true"), host="127.0.0.1", port = 5000, threaded = True)
